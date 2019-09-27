e0f2d15264ea08e0bcb331533644835e72219b73
Artjom Vejsel <akawolf0@gmail.com>
2019-09-28 02:18:17 +0300

    convert to utf-8

--- src/unix/unix_main.c
+++ src/unix/unix_main.c
@@ -4,7 +4,7 @@
 Return to Castle Wolfenstein single player GPL Source Code
 Copyright (C) 1999-2010 id Software LLC, a ZeniMax Media company. 
 
-This file is part of the Return to Castle Wolfenstein single player GPL Source Code (RTCW SP Source Code).  
+This file is part of the Return to Castle Wolfenstein single player GPL Source Code (RTCW SP Source Code).
 
 RTCW SP Source Code is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -432,8 +432,7 @@ void Sys_ConsoleInputInit() {
 				  characters  EOF,  EOL,  EOL2, ERASE, KILL, REPRINT,
 				  STATUS, and WERASE, and buffers by lines.
 		 ISIG: when any of the characters  INTR,  QUIT,  SUSP,  or
-				  DSUSP are received, generate the corresponding sig�
-				  nal
+				  DSUSP are received, generate the corresponding signal
 		*/
 		tc.c_lflag &= ~( ECHO | ICANON );
 		/*
